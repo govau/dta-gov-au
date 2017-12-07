c75ff560e25abd69f1647af7627c9fe0b224690b
Jesse Boyd <jesse.boyd@digital.gov.au>
2017-12-07 12:47:02 +1100

    Minor theme layout changes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-/* Stub script file. */
+(function($) {
+  'use strict';
+
+  Drupal.behaviors.dtagovau = {
+    attach: function(context, settings) {
+      $('main', context).once('behaviors').addClass('processed');
+    }
+  }
+}(jQuery));
