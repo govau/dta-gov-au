9846d72403cc864610a008cb37d580a6a383d9b6
Jesse Boyd <jesse.boyd@digital.gov.au>
2018-04-09 16:00:45 +1000

    Updated javascript NPM stuff.

--- js/scripts.js
+++ js/scripts.js
@@ -12,8 +12,10 @@
       $('a', context).once('smoothScroll').click(function(event) {
         var speed = 500;
         var href = $(this).attr("href").split('#')[1];
+        console.log(href);
         if (href) {
           $(this, context).once('behaviours').addClass('processed');
+          console.log($('#' + href));
           var position = $('#' + href).offset().top;
           $('html, body').animate({ scrollTop: position }, speed, "swing");
           event.preventDefault();
