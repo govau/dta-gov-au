53154496da5c449953a954116d57fdb88cd52c4f
KiriHoy <kiri.hoy@digital.gov.au>
2020-07-09 11:30:31 +1000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -231,7 +231,8 @@
   Drupal.behaviors.dtaFormFix = {
     // Add the red border when people tab out of the Mailchimp 'email' field.
     attach: function( context, settings ) {
-      $( 'form#mailchimp-signup-subscribe-block-sign-up-for-updates-form-1 .empty-required', context )
+
+      $( 'input .empty-required', context )
         .addClass( 'processed' )
         .on( 'submit', function() {
           if($("#edit-mergevars-email--2").val() == "" || $("#edit-mergevars-email--3").val() == "" || $("#edit-mergevars-email").val() == "" ){
